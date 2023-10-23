3b87c1d61d98027e5ade9589decb7b99a31786ca
Nere <Nereaberdugo2002@gmail.com>
2023-10-23 09:33:48 +0200

    Como calcular la propina parte 2 terminada

--- app/src/main/java/com/skye/practica2/MainActivity.kt
+++ app/src/main/java/com/skye/practica2/MainActivity.kt
@@ -2,53 +2,41 @@ package com.skye.practica2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.Switch
-import android.widget.TextView
-import android.widget.Toast
+
+import com.skye.practica2.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        //completado
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val calcularBoton:Button = findViewById(R.id.calculate_button);
-        val cantidad:EditText = findViewById(R.id.cost_of_service);
-        val radioGroup:RadioGroup=findViewById(R.id.tip_options)
-        val switch:Switch = findViewById(R.id.round_up_switch);
-        val resultado:TextView = findViewById(R.id.tip_result);
-        var porcentaje:Double = 0.0;
-        var resultadoOperacion:Double = 0.0;
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        calcularBoton.setOnClickListener {
-            val seleccionadoRadioButtonId = radioGroup.checkedRadioButtonId
+        binding.calculateButton.setOnClickListener{
+            calculateTip()
 
-            if (seleccionadoRadioButtonId != -1) {
-                // Obtiene el RadioButton seleccionado
-                val seleccionadoRadioButton = findViewById<RadioButton>(seleccionadoRadioButtonId)
-                when (seleccionadoRadioButton) {
-                    findViewById<RadioButton>(R.id.option_twenty_percent) -> porcentaje = 0.20
-                    findViewById<RadioButton>(R.id.option_eighteen_percent) -> porcentaje = 0.18
-                    findViewById<RadioButton>(R.id.option_fifteen_percent) -> porcentaje = 0.15
-                }
-                val cantidadTexto = cantidad.text.toString()
-                if (cantidadTexto.isNotEmpty()) {
-                    val cantidadDouble = cantidadTexto.toDouble()
-                    resultadoOperacion = cantidadDouble * porcentaje
-                    if (switch.isChecked) {
-                        resultadoOperacion = kotlin.math.ceil(resultadoOperacion)
-                    }
-                    resultado.text = resultadoOperacion.toString()
-                } else {
-                    Toast.makeText(this, "Ingresa una cantidad válida", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                // Si ningún RadioButton está seleccionado, puedes mostrar un mensaje de error o tomar alguna otra acción.
-                Toast.makeText(this, "Seleciona una opcion", Toast.LENGTH_SHORT).show();
-            }
         }
+    }
+    fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        val roundUp = binding.roundUpSwitch.isChecked
+        if (roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+        NumberFormat.getCurrencyInstance()
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
